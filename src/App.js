273629a42854657ac45a445dbd8ade3e6bc7ae0b
LucasMartins3031 <lucasms1002@gmail.com>
2022-09-24 23:28:18 -0300

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
 
+//import OlaMundo from "./components/OlaMundo"
+//import Estudante from './components/universidade/Estudante'
+//import {Vecna as V,Eleven, Will} from "./components/StrangerThings"
+//import * as StrangerThings from "./components/StrangerThings"
+//import Calculadora from './components/Calculadora';
+//import IMC from './components/IMC';
+//import Grupo from './components/vingadores/Grupo';
+//import Heroi from './components/vingadores/Heroi';
+//import Disciplina from './components/universidade/Disciplina';
+//import Pai from './components/paifilho/Pai';
+//import Contador from './components/estados/Contador';
+import VotaCidades from './components/estados/VotaCidades';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VotaCidades />
     </div>
   );
 }
+
+/*function App() {
+  return (
+    <div className="App">
+      <Disciplina titulo='Fundamentos de Programação'>
+        <Estudante
+          nome="Jefferson"
+          curso="Design Digital"
+          universidade="UFC Quixadá"
+        />
+        <Estudante
+          nome="Wladimir"
+          curso="Design Digital"
+          universidade="UFC Quixadá"
+        />
+      </Disciplina>
+    </div>
+  );
+}*/
+
+/*function App() {
+  return (
+    <div className="App">
+      <Grupo titulo='Vingadores'>
+        <Heroi nome='Capitão América' />
+        <Heroi nome='Hulk' />
+        <Heroi nome='Viúva Negra' />
+        <Heroi nome='Homem de Ferro' />
+        <h5>Fim do time!</h5>
+      </Grupo>
+    </div>
+  );
+}*/
+
+/*function App() {
+  return (
+    <div className="App">
+      <Estudante
+        nome="Jefferson"
+        curso="Design Digital"
+        universidade="UFC Quixadá" 
+      />
+      <StrangerThings.Vecna />
+      <Calculadora op="SUB" x={10} y={20} />
+      <IMC peso={80.10} altura ={1.7} />
+    </div>
+  );
+}*/
 
 export default App;
---
+++ src/components/estados/VotaCidades.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+
+const VotaCidades = () => {
+
+    const [quixada,setcontaquixada] = useState(0)
+    const [limoeiro,setcontalimoeiro] = useState(0)
+    const [crateus,setcontacrateus] = useState(0)
+
+    const votarquixada = ()=> {
+        setcontaquixada(quixada + 1)
+    }
+
+    const votarlimoeiro = ()=> {
+        setcontalimoeiro(limoeiro + 1)
+    }
+
+    const votarcrateus = ()=> {
+        setcontacrateus(crateus + 1)
+    }
+
+        const cidadecampea = ()=> {
+            if (crateus > limoeiro && crateus > quixada){
+                    alert("Cratéus Ficou no Top 1!!")
+            } else if (quixada > limoeiro && quixada > crateus){
+                    alert("Quixadá Ficou no Top 1!!")
+            } else if (limoeiro > crateus && limoeiro > quixada){
+                    alert("Límoeiro do Norte Ficou no Top 1!!")
+            } else {
+                    alert("Houver um empate mais de uma cidade no top 1!!")
+            }
+        }
+
+    return (
+        <div>
+            <div>
+                <h2>Quixadá: {quixada}</h2>
+                <h2>Limoeiro do Norte: {limoeiro}</h2>
+                <h2>Crateús: {crateus}</h2>
+            </div>
+            <div>
+                <button onClick={votarquixada}>Votar Quixadá</button>
+                <button onClick={votarlimoeiro}>Votar Limoeiro do Norte</button>
+                <button onClick={votarcrateus}>Votar Crateús</button>
+                <br />
+                <button onClick={cidadecampea}>Encerrar Votação</button>
+            </div>
+        </div>
+
+    )
+
+}
+
+export default VotaCidades
